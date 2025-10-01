c41b7e6aa58bef23d31d498324f6d88d54b205aa
24vasilekk <24vasilekk@gmail.com>
2025-10-02 01:57:15 +0300

    Fix Railway deployment

--- server.js
+++ server.js
@@ -4,23 +4,17 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files
 app.use(express.static(__dirname));
-
-// Serve images
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-// Main route
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// 404 handler
 app.use((req, res) => {
   res.status(404).send('Page not found');
 });
 
 app.listen(PORT, () => {
   console.log(`VALUES магазин запущен на порту ${PORT}`);
-  console.log(`Откройте http://localhost:${PORT} в браузере`);
 });
